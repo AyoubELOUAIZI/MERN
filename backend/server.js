3cc9d5015c86879690ff9f3243d6a9b06f86b33a
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-14 14:23:26 +0000

    connecting whith the mongodb db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const { request } = require("express");
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/wourkouts');
-
 const express = require("express");
 const app = express();
+
+
 const PORT = process.env.PORT || 3000;
+const connectionString = process.env.MONGO_URI;
+
+
+
+
+
+
+
+
 //routes
 app.use('/api/workout', workoutRoutes);
 
@@ -21,16 +31,21 @@ app.get("/", (req, res) => {
 
 
 
+//connecting with mongo db and then make the server listening in the port
+mongoose.connect(connectionString).then(() => {
+    console.log("\n********************Successfully connected to the database.");
+
+    //listen for requests
+    app.listen(PORT, () => {
+        console.log(`Server is running on port 4010 or 3000 Click http://localhost:${PORT}`);
+    });
+
+
+})
+    .catch(err => {
+        console.log("Error connecting to the database: ", err);
+    });
 
 
 
 
-
-
-
-
-
-//listen for requests
-app.listen(PORT, () => {
-    console.log(`Server is running on port 4010 or 3000 Click http://localhost:${PORT}`);
-});
