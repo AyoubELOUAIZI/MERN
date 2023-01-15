37e4c91c053e9384a97c36c570c82801e80a51cf
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-15 14:19:51 +0000

    the frontend now is start korking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//pages and components
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-     <div className='pages'>
-        <Route>
-          <Route 
-          path='/'
-       //   element={ }
-          />
-        </Route>
-     </div>
-     </BrowserRouter>
+      <BrowserRouter>
+        <div className='pages'>
+          <Routes>
+            <Route
+              path='/'
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
