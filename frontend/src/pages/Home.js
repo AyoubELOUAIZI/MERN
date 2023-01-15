112296f37bcb5a8c4507c5b5b7162c1442b0f4cc
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-15 18:58:54 +0000

    now it is fetching and printing data from mongodb db to backend to dom react

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails';
+
+
 const Home = () => {
+    // state variable to hold the data
+    const [workouts, setWorkouts] = useState(null);
+
+    // useEffect hook to fetch data from the server
+    useEffect(() => {
+        // function to fetch workouts data from the server
+        const fetchWorkouts = async () => {
+            try {
+                // make a GET request to the server
+                const response = await fetch('/api/workout/');
+                // parse the response as JSON
+                const json = await response.json();
+                // update the state with the fetched data
+                if (response.ok) {
+                    setWorkouts(json);
+                }
+            } catch (error) {
+                //handle any errors that occur during the fetch
+                console.log(error);
+            }
+        }
+        // call the function to fetch the data
+        fetchWorkouts();
+    }, []);
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map(workout => (
+                    // render a div element for each item in the data
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }
